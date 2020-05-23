cdaf2c77110f30441b10dcf6bc78b497f6852a21
Shaleen Jha <shaleenjha11@gmail.com>
2020-05-23 12:18:09 +0530

    Third Commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// alert("Hello!");
+
+var btn_reg = document.querySelector("#register");
+btn_reg.addEventListener("click", function(){
+	alert("Sign Up!!!")
+});
+
+var btn_lgn = document.querySelector("#login");
+btn_lgn.addEventListener("click",function(){
+	alert("Sign In!!!")
+});
